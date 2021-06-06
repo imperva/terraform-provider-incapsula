f65f03d95e5c01e9699768ecc1a78f1036d5931a
moshe.hagoel <moshe.hagoel@imperva.com>
2021-06-06 09:44:37 +0300

    resource_origin_pop - implementing read and import for the resource. Read and import are using the list dcs API which starting 06/06/21 will return the originPop as part of its response. NOTICE: there is an additional parameter "site_id" required in the origin pop configuration file. The request for setting originPop to NONE requires sending no value in the request, that's why in the function setOriginPop the URL is constructed by the case. (modify vs "delete")

--- incapsula/resource_origin_pop.go
+++ incapsula/resource_origin_pop.go
@@ -2,21 +2,21 @@ package incapsula
 
 import (
 	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"log"
 	"strconv"
 	"strings"
-	"time"
-
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func resourceOriginPOP() *schema.Resource {
 	return &schema.Resource{
-		Create:   resourceOriginPOPUpdate,
-		Read:     resourceOriginPOPRead,
-		Update:   resourceOriginPOPUpdate,
-		Delete:   resourceOriginPOPDelete,
-		Importer: nil,
+		Create: resourceOriginPOPUpdate,
+		Read:   resourceOriginPOPRead,
+		Update: resourceOriginPOPUpdate,
+		Delete: resourceOriginPOPDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
 
 		Schema: map[string]*schema.Schema{
 			// Required Arguments
@@ -25,6 +25,11 @@ func resourceOriginPOP() *schema.Resource {
 				Type:        schema.TypeInt,
 				Required:    true,
 				ForceNew:    true,
+			},
+			"site_id": {
+				Description: "Numeric identifier of the site.",
+				Type:        schema.TypeInt,
+				Required:    true,
 			},
 			"origin_pop": {
 				Description: "The Origin POP code (must be lowercase), e.g: iad. Note, this field is create/update only. Reads are not supported as the API doesn't exist yet. Note that drift may happen.",
@@ -46,6 +51,7 @@ func resourceOriginPOP() *schema.Resource {
 func resourceOriginPOPUpdate(d *schema.ResourceData, m interface{}) error {
 	client := m.(*Client)
 	dcID := d.Get("dc_id").(int)
+	siteID := d.Get("site_id").(int)
 	originPOP := d.Get("origin_pop").(string)
 
 	log.Printf("[INFO] Setting Incapsula origin POP: %s for data center: %d\n", originPOP, dcID)
@@ -57,17 +63,64 @@ func resourceOriginPOPUpdate(d *schema.ResourceData, m interface{}) error {
 		return err
 	}
 
-	// Set the ID
-	d.SetId(strconv.FormatInt(time.Now().Unix(), 10))
 	log.Printf("[INFO] Set Incapsula origin POP: %s for data center: %d\n", originPOP, dcID)
 
-	return nil
+	syntheticID := fmt.Sprintf("%d/%d", siteID, dcID)
+	d.SetId(syntheticID)
+
+	return resourceOriginPOPRead(d, m)
 }
 
 func resourceOriginPOPRead(d *schema.ResourceData, m interface{}) error {
+	// Implement by reading the ListDataCentersResponse for the data centers
+	client := m.(*Client)
+	siteID := strings.Split(d.Id(), "/")[0]
+	dcID := strings.Split(d.Id(), "/")[1]
+
+	listDataCentersResponse, err := client.ListDataCenters(siteID)
+
+	if err != nil {
+		log.Printf("[ERROR] Could not read origin POP for data center: %s, site: %s %s\n", dcID, siteID, err)
+		return err
+	}
+
+	found := false
+
+	for _, dataCenter := range listDataCentersResponse.DCs {
+		if dataCenter.ID == dcID {
+			originPop := dataCenter.OriginPop
+			if originPop != "" {
+				siteIDInteger, _ := strconv.Atoi(siteID)
+				dcIDInteger, _ := strconv.Atoi(dcID)
+				d.Set("site_id", siteIDInteger)
+				d.Set("dc_id", dcIDInteger)
+				d.Set("origin_pop", originPop)
+				syntheticID := fmt.Sprintf("%s/%s", siteID, dcID)
+				d.SetId(syntheticID)
+				found = true
+				break
+			}
+		}
+	}
+
+	if !found {
+		d.SetId("")
+	}
+
 	return nil
 }
 
 func resourceOriginPOPDelete(d *schema.ResourceData, m interface{}) error {
+	client := m.(*Client)
+	dcID := d.Get("dc_id").(int)
+	err := client.SetOriginPOP(dcID, "")
+
+	if err != nil {
+		log.Printf("[ERROR] Could not delete Incapsula origin POP for data center: %d: %s\n", dcID, err)
+		return err
+	}
+
+	log.Printf("[INFO] Deleted Incapsula origin POP for data center: %d\n", dcID)
+
 	return nil
 }
