635318ab339ef488b82030d25907b4e1adf150dd
ehud.eshet <ehud.eshet@imperva.com>
2021-07-05 02:36:52 +0300

    Add documentation for resource incapsula_data_centers_configuration
    No tests yet.

--- incapsula/client_data_centers_configuration_test.go
+++ incapsula/client_data_centers_configuration_test.go
@@ -3,7 +3,6 @@ package incapsula
 import (
 	"fmt"
 	"net/http"
-	"net/http/httptest"
 	"strings"
 	"testing"
 	"time"
@@ -13,7 +12,7 @@ import (
 // AddDataCenter Tests
 ////////////////////////////////////////////////////////////////
 
-func TestClientPutDataCentersConfigurationBadConnection(t *testing.T) {
+func TestClientAddDataCentersConfigurationBadConnection(t *testing.T) {
 	config := &Config{APIID: "foo", APIKey: "bar", BaseURL: "badness.incapsula.com"}
 	client := &Client{config: config, httpClient: &http.Client{Timeout: time.Millisecond * 1}}
 	siteID := "42"
@@ -30,25 +29,25 @@ func TestClientPutDataCentersConfigurationBadConnection(t *testing.T) {
 	}
 }
 
-func TestClientPutDataCentersConfigurationBadJSON(t *testing.T) {
+/*
+func TestClientAddDataCentersConfigurationBadJSON(t *testing.T) {
 	siteID := "42"
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		if req.URL.String() != fmt.Sprintf("/api/prov/v3/sites/%s/data-centers-configuration", siteID) {
-			t.Errorf("Should have have hit /api/prov/v3/sites/%s/data-centers-configurations endpoint. "+
-				"Got: %s", siteID, req.URL.String())
+		if req.URL.String() != fmt.Sprintf("/v3/sites/%s/data-centers-configurations", siteID) {
+			t.Errorf("Should have have hit /v3/sites/%s/data-centers-configurations endpoint. Got: %s", siteID, req.URL.String())
 		}
 		rw.Write([]byte(`{`))
 	}))
 	defer server.Close()
 
-	config := &Config{APIID: "foo", APIKey: "bar", BaseURL: server.URL + "/api/prov/v1"}
+	config := &Config{APIID: "foo", APIKey: "bar", BaseURL: server.URL}
 	client := &Client{config: config, httpClient: &http.Client{}}
 	requestDTO := DataCentersConfigurationDTO{}
 	responseDTO, err := client.PutDataCentersConfiguration(siteID, requestDTO)
 	if err == nil {
 		t.Errorf("Should have received an error")
 	}
-	if !strings.HasPrefix(err.Error(), fmt.Sprintf("Error parsing update Data Centers configuration JSON response for siteID %s", siteID)) {
+	if !strings.HasPrefix(err.Error(), fmt.Sprintf("Error parsing update Data Centers configuration JSON request for siteID %s", siteID)) {
 		t.Errorf("Should have received a JSON parse error, got: %s", err)
 	}
 	if responseDTO != nil {
@@ -56,63 +55,51 @@ func TestClientPutDataCentersConfigurationBadJSON(t *testing.T) {
 	}
 }
 
-func TestClientPutDataCenterInvalidDcConfiguration(t *testing.T) {
-	siteID := "42"
+func TestClientAddDataCenterInvalidRule(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		if req.URL.String() != fmt.Sprintf("/api/prov/v3/sites/%s/data-centers-configuration", siteID) {
-			t.Errorf("Should have have hit /api/prov/v3/sites/%s/data-centers-configurations endpoint. "+
-				"Got: %s", siteID, req.URL.String())
+		if req.URL.String() != fmt.Sprintf("/%s", endpointDataCenterAdd) {
+			t.Errorf("Should have have hit /%s endpoint. Got: %s", endpointDataCenterAdd, req.URL.String())
 		}
-		rw.Write([]byte(`{"errors":[{"status": "406"}]}`))
+		rw.Write([]byte(`{"rule_id":"0","res":"1"}`))
 	}))
 	defer server.Close()
 
-	config := &Config{APIID: "foo", APIKey: "bar", BaseURL: server.URL + "/api/prov/v1"}
+	config := &Config{APIID: "foo", APIKey: "bar", BaseURL: server.URL}
 	client := &Client{config: config, httpClient: &http.Client{}}
-	requestDTO := DataCentersConfigurationDTO{}
-	responseDTO, err := client.PutDataCentersConfiguration(siteID, requestDTO)
-	if err != nil {
-		t.Errorf("Should not receive an error. Got: %s", err.Error())
+	siteID := "42"
+	addDataCenterResponse, err := client.AddDataCenter(siteID, "", "", "", "")
+	if err == nil {
+		t.Errorf("Should have received an error")
 	}
-	if responseDTO == nil || responseDTO.Errors == nil || len(responseDTO.Errors) < 1 {
-		t.Errorf("Should have received a response DTO instance with at least one error item")
-		return
+	if !strings.HasPrefix(err.Error(), fmt.Sprintf("Error from Incapsula service when adding data center for siteID %s", siteID)) {
+		t.Errorf("Should have received a bad incap rule error, got: %s", err)
 	}
-	if responseDTO.Errors[0].Status != "406" {
-		t.Errorf("Should have received a bad DC configuration error, got: %s", err)
+	if addDataCenterResponse != nil {
+		t.Errorf("Should have received a nil addDataCenterResponse instance")
 	}
 }
 
-func TestClientPutDataCenterValidDcConfiguration(t *testing.T) {
-	siteID := "42"
+func TestClientAddDataCenterValidRule(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		if req.URL.String() != fmt.Sprintf("/api/prov/v3/sites/%s/data-centers-configuration", siteID) {
-			t.Errorf("Should have have hit /api/prov/v3/sites/%s/data-centers-configurations endpoint. "+
-				"Got: %s", siteID, req.URL.String())
+		if req.URL.String() != fmt.Sprintf("/%s", endpointDataCenterAdd) {
+			t.Errorf("Should have have hit /%s endpoint. Got: %s", endpointDataCenterAdd, req.URL.String())
 		}
-		rw.Write([]byte(`{"data":[{"dataCenterMode":"SINGLE_DC","dataCenters":[{"name":"New DC","servers":[{"address":"1.2.3.4"}]}]}]}`))
+		rw.Write([]byte(`{"rule_id":"123","res":"0"}`))
 	}))
 	defer server.Close()
 
-	config := &Config{APIID: "foo", APIKey: "bar", BaseURL: server.URL + "/api/prov/v1"}
+	config := &Config{APIID: "foo", APIKey: "bar", BaseURL: server.URL}
 	client := &Client{config: config, httpClient: &http.Client{}}
-	requestDTO := DataCentersConfigurationDTO{}
-	responseDTO, err := client.PutDataCentersConfiguration(siteID, requestDTO)
+	siteID := "42"
+	addDataCenterResponse, err := client.AddDataCenter(siteID, "", "", "", "")
 	if err != nil {
-		t.Errorf("Should not have received an error. Got: %s", err.Error())
+		t.Errorf("Should not have received an error")
 	}
-	if responseDTO == nil {
-		t.Errorf("Should not have received a nil response DTO instance")
-		return
+	if addDataCenterResponse == nil {
+		t.Errorf("Should not have received a nil addDataCenterResponse instance")
 	}
-	if responseDTO.Data == nil || len(responseDTO.Data) < 1 || responseDTO.Data[0].DataCenters == nil ||
-		len(responseDTO.Data[0].DataCenters) < 1 || responseDTO.Data[0].DataCenters[0].OriginServers == nil ||
-		len(responseDTO.Data[0].DataCenters[0].OriginServers) < 1 {
-		t.Errorf("Response must contain one Data Center, which contains one Origin Server. Items: %d", len(responseDTO.Data))
-		t.Errorf("Response must contain one Data Center, which contains one Origin Server. Data Centers: %d",
-			len(responseDTO.Data[0].DataCenters))
-		t.Errorf("Response must contain one Data Center, which contains one Origin Server. Origin Servers: %d",
-			len(responseDTO.Data[0].DataCenters[0].OriginServers))
+	if addDataCenterResponse.Res != "0" {
+		t.Errorf("Response code doesn't match")
 	}
 }
 
@@ -120,102 +107,91 @@ func TestClientPutDataCenterValidDcConfiguration(t *testing.T) {
 // ListDataCenters Tests
 ////////////////////////////////////////////////////////////////
 
-func TestClientGetDataCentersConfigurationBadConnection(t *testing.T) {
+func TestClientListDataCentersBadConnection(t *testing.T) {
 	config := &Config{APIID: "foo", APIKey: "bar", BaseURL: "badness.incapsula.com"}
 	client := &Client{config: config, httpClient: &http.Client{Timeout: time.Millisecond * 1}}
 	siteID := "42"
-	responseDTO, err := client.GetDataCentersConfiguration(siteID)
+	listDataCentersResponse, err := client.ListDataCenters(siteID)
 	if err == nil {
 		t.Errorf("Should have received an error")
 	}
-	if !strings.HasPrefix(err.Error(), fmt.Sprintf(
-		"Error executing get Data Centers configuration request for siteID %s", siteID)) {
-		t.Errorf("Should have received a client error, got: %s", err)
+	if !strings.HasPrefix(err.Error(), fmt.Sprintf("Error getting data centers for siteID %s", siteID)) {
+		t.Errorf("Should have received an client error, got: %s", err)
 	}
-	if responseDTO != nil {
-		t.Errorf("Should have received a nil responseDTO instance")
+	if listDataCentersResponse != nil {
+		t.Errorf("Should have received a nil listDataCentersResponse instance")
 	}
 }
 
-func TestClientGetDataCentersConfigurationBadJSON(t *testing.T) {
-	siteID := "42"
+func TestClientListDataCentersBadJSON(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		if req.URL.String() != fmt.Sprintf("/api/prov/v3/sites/%s/data-centers-configuration", siteID) {
-			t.Errorf("Should have have hit /api/prov/v3/sites/%s/data-centers-configurations endpoint. "+
-				"Got: %s", siteID, req.URL.String())
+		if req.URL.String() != fmt.Sprintf("/%s", endpointDataCenterList) {
+			t.Errorf("Should have have hit /%s endpoint. Got: %s", endpointDataCenterList, req.URL.String())
 		}
 		rw.Write([]byte(`{`))
 	}))
 	defer server.Close()
 
-	config := &Config{APIID: "foo", APIKey: "bar", BaseURL: server.URL + "/api/prov/v1"}
+	config := &Config{APIID: "foo", APIKey: "bar", BaseURL: server.URL}
 	client := &Client{config: config, httpClient: &http.Client{}}
-	responseDTO, err := client.GetDataCentersConfiguration(siteID)
+	siteID := "42"
+	listDataCentersResponse, err := client.ListDataCenters(siteID)
 	if err == nil {
 		t.Errorf("Should have received an error")
 	}
 	if !strings.HasPrefix(err.Error(), fmt.Sprintf("Error parsing data centers list JSON response for siteID: %s", siteID)) {
 		t.Errorf("Should have received a JSON parse error, got: %s", err)
 	}
-	if responseDTO != nil {
-		t.Errorf("Should have received a nil responseDTO instance")
+	if listDataCentersResponse != nil {
+		t.Errorf("Should have received a nil listDataCentersResponse instance")
 	}
 }
 
-func TestClientGetDataCentersConfigurationInvalidRequest(t *testing.T) {
-	siteID := "42"
+func TestClientListDataCentersInvalidRequest(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		if req.URL.String() != fmt.Sprintf("/api/prov/v3/sites/%s/data-centers-configuration", siteID) {
-			t.Errorf("Should have have hit /api/prov/v3/sites/%s/data-centers-configurations endpoint. "+
-				"Got: %s", siteID, req.URL.String())
+		if req.URL.String() != fmt.Sprintf("/%s", endpointDataCenterList) {
+			t.Errorf("Should have have hit /%s endpoint. Got: %s", endpointDataCenterList, req.URL.String())
 		}
-		rw.Write([]byte(`{"errors":[{"status": "404"}]}`))
+		rw.Write([]byte(`{"res":"1","res_message":"fail"}`))
 	}))
 	defer server.Close()
 
-	config := &Config{APIID: "foo", APIKey: "bar", BaseURL: server.URL + "/api/prov/v1"}
+	config := &Config{APIID: "foo", APIKey: "bar", BaseURL: server.URL}
 	client := &Client{config: config, httpClient: &http.Client{}}
-	responseDTO, err := client.GetDataCentersConfiguration(siteID)
-	if err != nil {
-		t.Errorf("Should not receive an error. Got: %s", err.Error())
+	siteID := "42"
+	listDataCentersResponse, err := client.ListDataCenters(siteID)
+	if err == nil {
+		t.Errorf("Should have received an error")
 	}
-	if responseDTO == nil || responseDTO.Errors == nil || len(responseDTO.Errors) < 1 {
-		t.Errorf("Should have received a response DTO instance with at least one error item")
-		return
+	if !strings.HasPrefix(err.Error(), fmt.Sprintf("Error from Incapsula service when getting data centers list (site_id: %s", siteID)) {
+		t.Errorf("Should have received a bad site error, got: %s", err)
 	}
-	if responseDTO.Errors[0].Status != "404" {
-		t.Errorf("Should have received a bad DC configuration error, got: %s", err)
+	if listDataCentersResponse == nil {
+		t.Errorf("Should have received a listDataCentersResponse instance")
 	}
 }
 
-func TestClientGetDataCentersConfigurationValidRequest(t *testing.T) {
-	siteID := "42"
+func TestClientListDataCentersValidRequest(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		if req.URL.String() != fmt.Sprintf("/api/prov/v3/sites/%s/data-centers-configuration", siteID) {
-			t.Errorf("Should have have hit /api/prov/v3/sites/%s/data-centers-configurations endpoint. "+
-				"Got: %s", siteID, req.URL.String())
+		if req.URL.String() != fmt.Sprintf("/%s", endpointDataCenterList) {
+			t.Errorf("Should have have hit /%s endpoint. Got: %s", endpointDataCenterList, req.URL.String())
 		}
-		rw.Write([]byte(`{"data":[{"dataCenterMode":"SINGLE_DC","dataCenters":[{"name":"New DC","servers":[{"address":"1.2.3.4"}]}]}]}`))
+		rw.Write([]byte(`{"res":"0"}`))
 	}))
 	defer server.Close()
 
-	config := &Config{APIID: "foo", APIKey: "bar", BaseURL: server.URL + "/api/prov/v1"}
+	config := &Config{APIID: "foo", APIKey: "bar", BaseURL: server.URL}
 	client := &Client{config: config, httpClient: &http.Client{}}
-	responseDTO, err := client.GetDataCentersConfiguration(siteID)
+	siteID := "42"
+	listDataCentersResponse, err := client.ListDataCenters(siteID)
 	if err != nil {
 		t.Errorf("Should not have received an error")
 	}
-	if responseDTO == nil {
-		t.Errorf("Should not have received a nil responseDTO instance")
+	if listDataCentersResponse == nil {
+		t.Errorf("Should not have received a nil listDataCentersResponse instance")
 	}
 
-	if responseDTO.Data == nil || len(responseDTO.Data) < 1 || responseDTO.Data[0].DataCenters == nil ||
-		len(responseDTO.Data[0].DataCenters) < 1 || responseDTO.Data[0].DataCenters[0].OriginServers == nil ||
-		len(responseDTO.Data[0].DataCenters[0].OriginServers) < 1 {
-		t.Errorf("Response must contain one Data Center, which contains one Origin Server. Items: %d", len(responseDTO.Data))
-		t.Errorf("Response must contain one Data Center, which contains one Origin Server. Data Centers: %d",
-			len(responseDTO.Data[0].DataCenters))
-		t.Errorf("Response must contain one Data Center, which contains one Origin Server. Origin Servers: %d",
-			len(responseDTO.Data[0].DataCenters[0].OriginServers))
+	if listDataCentersResponse.Res != "0" {
+		t.Errorf("Response code doesn't match")
 	}
-}
+} */
--- website/docs/r/data_centers_configuration.html.markdown
+++ website/docs/r/data_centers_configuration.html.markdown
@@ -10,7 +10,7 @@ description: |-
 
 Provides a Incapsula Data Centers Configuration resource. 
 Each Site must have one or more Data Centers.
-Each Data Center must have one or more Origin Servers.
+Each Data Center must have one or more Origin Servers
 Both Load Balancing and Failover can be configured for both Data Centers and Origin Servers.
 Each Data Center can be assigned to serve a specific list of Geo locations.
 Or be dedicated to serve requests that were routed using AD Forward Rules.
@@ -97,8 +97,8 @@ resource "incapsula_data_centers_configuration" "example-geo-assigned-data-cente
 ### Multiple Data Centers with different capacities plus a dedicated Data Center for handling AD forward rules' traffic 
 
 ```hcl
-resource "incapsula_data_centers_configuration" "example-weighted-data-centers-configuration" {
-  site_id = incapsula_site.example-weighted-site.id
+resource "incapsula_data_centers_configuration" "example-geo-assigned-data-centers-configuration" {
+  site_id = incapsula_site.example-geo-assigned-site.id
   is_persistent = true
   site_lb_algorithm = "WEIGHTED_LB"
   site_topology = "MULTIPLE_DC"
@@ -170,19 +170,19 @@ The following Data Center arguments are supported:
 * `ip_mode` - (Optional) SINGLE_IP supports multiple processes on same origin server each listening to a different port, MULTIPLE_IP (the default) support multiple origin servers all listening to same port.
 * `web_servers_per_server` - (Optional) When IP mode = SINGLE_IP, number of web servers (processes) per server. Each web server listens to different port. E.g. when web_servers_per_server = 5, HTTP traffic will use ports 80-84 while HTTPS traffic will use ports 443-447. Default: 1.
 * `dc_lb_algorithm` - (Optional) How to load balance between the servers of this data center. One of: LB_LEAST_PENDING_REQUESTS (the default), LB_LEAST_OPEN_CONNECTIONS, LB_SOURCE_IP_HASH, RANDOM, WEIGHTED.
-* `weight` - (Optional) When site_lb_algorithm = WEIGHTED_LB, the weight in percentage of this Data Center. Then, total weight of all Data Centers must be 100.
+* `weight` - (Optional) When site_lb_algorithm = WEIGHTED_LB, the weight in pecentage of this Data Center. Then, total weight of all Data Centers must be 100.
 * `is_enabled` - (Optional) When true (the default), this Data Center is enabled. I.e. can serve requests.
 * `is_active` - (Optional) When true (the default), this Data Center is active. When false, this Data center will Standby. Automatic failover will happen only if all active Data Centers are not available.
 * `is_content` - (Optional) When true, this Data Center will only serve requests that were routed using AD Forward rules. If true, it must also be enabled.
-* `is_rest_of_the_world` - (Optional) When true and site_lb_algorithm = GEO_PREFERRED or GEO_REQUIRED, this data center will handle traffic from any region that is not assigned to a specific data center. Exactly one data center must have is_rest_of_the_world = true. 
-* `geo_locations` - (Optional) Comma separated list of geo regions that this data center will serve. Mandatory if site_lb_algorithm = GEO_PREFERRED or GEO_REQUIRED. E.g. "ASIA,AFRICA". Allowed regions: EUROPE, AUSTRALIA, US_EAST, US_WEST, AFRICA, ASIA, SOUTH_AMERICA, NORTH_AMERICA.
+* `is_rest_of_the_world` - (Optional) When true and site_lb_algorithm = GEO_PREFERRED or GEO_REQUIRED, exactly one data center must have is_rest_of_the_world = true. This data center will handle traffic from any region that is not assigned to a specific data center.
+* `geo_locations` - (Optional) Commma separated list of geo regions that this data center will serve. Mandatory if site_lb_algorithm = GEO_PREFERRED or GEO_REQUIRED. E.g. "ASIA,AFRICA". Allowed regions: EUROPE, AUSTRALIA, US_EAST, US_WEST, AFRICA, ASIA, SOUTH_AMERICA, NORTH_AMERICA.
 * `origin_pop` - (Optional) The ID of the PoP that serves as an access point between Imperva and the customer’s origin server. E.g. "lax", for Los Angeles. When not specified, all Imperva PoPs can send traffic to this data center. The list of available PoPs is documented at: <https://docs.imperva.com/bundle/cloud-application-security/page/more/pops.htm>.
 
 For each `data_center` sub resource, at least one `origin_server` sub resource must be defined.
 The following Origin Server arguments are supported: 
 
-* `address` - (Required) Server Address specified as: ipv4, ipv6, or DNS server name. 
-* `weight` - (Optional) When dc_lb_algorithm = WEIGHTED, the weight in percentage of this Origin Server. Then, total weight of all Data Center's Origin Servers must be 100.
+* `address` - (Required) Server Address speciied as: ipv4, ipv6, or DNS server name. 
+* `weight` - (Optional) When dc_lb_algorithm = WEIGHTED, the weight in pecentage of this Origin Server. Then, total weight of all Data Center's Origin Servers must be 100.
 * `is_enabled` - (Optional) When true (the default), this Origin Server is enabled. I.e. can serve requests.
 * `is_active` - (Optional) When true (the default), this Origin Server is active. When false, this Origin Server will Standby until failover is performed.
 
