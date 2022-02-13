89ae6f452fc95537984039c0852548f25cbd4dc5
katrin.polit <katrin.polit@gmail.com>
2022-02-13 09:43:19 +0200

    txt-record resource - bump version to 3.4.0

--- incapsula/client.go
+++ incapsula/client.go
@@ -25,7 +25,7 @@ type Client struct {
 func NewClient(config *Config) *Client {
 	client := &http.Client{}
 
-	return &Client{config: config, httpClient: client, providerVersion: "3.3.0"}
+	return &Client{config: config, httpClient: client, providerVersion: "3.4.0"}
 }
 
 // Verify checks the API credentials
