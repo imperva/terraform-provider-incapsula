5e0352ed78c41a62e82d64a62b4b8422f7c36d21
tal.feferberg <talf.fb@gmail.com>
2022-05-09 16:15:31 +0300

    Fix Notification center slash in path issue

--- incapsula/client.go
+++ incapsula/client.go
@@ -106,6 +106,7 @@ func (c *Client) DoJsonAndQueryParamsRequestWithHeaders(method string, url strin
 		q.Add(name, value)
 	}
 	req.URL.RawQuery = q.Encode()
+	log.Printf("[DEBUG] The request: %+v", req)
 
 	SetHeaders(c, req, contentTypeApplicationJson, nil)
 
