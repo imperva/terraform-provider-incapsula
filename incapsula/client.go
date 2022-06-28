2bc27d43805ecd7d5fcc8c230a6260e44fe3a0ad
pablo-s-martinez <pablosebastian.m@imperva.com>
2022-06-28 09:26:41 +0300

    added new resource: incapsula_waf_log_setup

--- incapsula/client.go
+++ incapsula/client.go
@@ -26,7 +26,7 @@ type Client struct {
 func NewClient(config *Config) *Client {
 	client := &http.Client{}
 
-	return &Client{config: config, httpClient: client, providerVersion: "3.7.0"}
+	return &Client{config: config, httpClient: client, providerVersion: "3.6.0"}
 }
 
 // Verify checks the API credentials
