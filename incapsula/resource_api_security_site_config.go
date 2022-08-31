4f3cedaaff38d6b23956193a9c6cc9adc16d54c8
Bracha Yalon <38453612+BrachaY@users.noreply.github.com>
2022-08-31 11:53:06 +0300

    incapsula_api_security_site_config: make is_automatic_discovery_api_i… (#205)
    * incapsula_api_security_site_config: make is_automatic_discovery_api_integration_enabled optional like in the API
    
    * incapsula_api_security_site_config: make is_automatic_discovery_api_integration_enabled optional like in the API

--- incapsula/resource_api_security_site_config.go
+++ incapsula/resource_api_security_site_config.go
@@ -36,7 +36,7 @@ func resourceApiSecuritySiteConfig() *schema.Resource {
 			"is_automatic_discovery_api_integration_enabled": {
 				Description: "Parameter shows whether automatic API discovery is enabled",
 				Type:        schema.TypeBool,
-				Required:    false,
+				Optional:    true,
 			},
 
 			//Optional
