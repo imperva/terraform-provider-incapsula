633e644d9bf0683e30a0c344362ba140dee402ef
dotan.raz <dotan.raz@imperva.com>
2022-10-31 21:15:14 +0200

    convert domain resource to work with bulk operation

--- incapsula/resource_domain.go
+++ incapsula/resource_domain.go
@@ -1,21 +1,23 @@
 package incapsula
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"strconv"
+	"log"
 )
 
 func resourceDomain() *schema.Resource {
 	return &schema.Resource{
 		Read:   resourceDomainRead,
-		Create: resourceDomainCreate,
+		Create: resourceDomainUpdate,
 		Delete: resourceDomainDelete,
 		Update: resourceDomainUpdate,
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
 		},
 		Schema: map[string]*schema.Schema{
-			// Required Arguments
 			"site_id": {
 				Description: "Numeric identifier of the account to operate on. If not specified, operation will be performed on the account identified by the authentication parameters.",
 				Type:        schema.TypeString,
@@ -23,43 +25,160 @@ func resourceDomain() *schema.Resource {
 			},
 			"domain": {
 				Description: "",
-				Type:        schema.TypeString,
 				Required:    true,
+				Type:        schema.TypeSet,
+				Set:         getHashFromDomain,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"domain_name": {
+							Type:        schema.TypeString,
+							Description: "domain",
+							Required:    true,
+						},
+						"id": {
+							Type:        schema.TypeInt,
+							Description: "domain id",
+							Computed:    true,
+						},
+						"status": {
+							Type:        schema.TypeString,
+							Description: "status",
+							Computed:    true,
+						},
+					}},
 			},
 		},
 	}
 }
 
-func resourceDomainCreate(d *schema.ResourceData, m interface{}) error {
+func getHashFromDomain(v interface{}) int {
+	var buf bytes.Buffer
+	m := v.(map[string]interface{})
+
+	if v, ok := m["domain_name"]; ok {
+		buf.WriteString(fmt.Sprintf("%s-", v.(string)))
+	}
+	return PositiveHash(buf.String())
+}
+
+func populateFromResourceToDTO(d *schema.ResourceData) []SiteDomainDetails {
+	domainsConf := d.Get("domain").(*schema.Set)
+	var siteDomainDetails = make([]SiteDomainDetails, len(domainsConf.List()))
+	var domainInd int = 0
+	for _, domain := range domainsConf.List() {
+		domainItem := domain.(map[string]interface{})
+		siteDomainDetails[domainInd] = SiteDomainDetails{}
+		if attr, ok := domainItem["domain_name"]; ok && attr != "" {
+			siteDomainDetails[domainInd].Domain = attr.(string)
+		}
+		domainInd++
+	}
+	return siteDomainDetails
+}
+
+func resourceDomainUpdate(d *schema.ResourceData, m interface{}) error {
 	client := m.(*Client)
 	siteID := d.Get("site_id").(string)
-	domain := d.Get("domain").(string)
-
-	domainManagementData, err := client.AddDomainToSite(siteID, domain)
+	siteDomainDetails := populateFromResourceToDTO(d)
+	siteDomainDetailsDto, err := client.BulkUpdateDomainsToSite(siteID, siteDomainDetails)
 	if err != nil {
 		return err
 	}
 
-	d.SetId(strconv.Itoa(domainManagementData.Id))
+	if siteDomainDetailsDto.Errors != nil && len(siteDomainDetailsDto.Errors) > 0 {
+		if siteDomainDetailsDto.Errors[0].Status == "404" || siteDomainDetailsDto.Errors[0].Status == "400" {
+			log.Printf("[INFO] Operation not allowed: %s\n", siteDomainDetailsDto.Errors)
+			d.SetId("")
+			return nil
+		}
+
+		if siteDomainDetailsDto.Errors[0].Status == "500" {
+			log.Printf("[INFO] Internal Server Error: %s\n", siteDomainDetailsDto.Errors)
+			d.SetId("")
+			return nil
+		}
+
+		out, err := json.Marshal(siteDomainDetailsDto.Errors)
+		if err != nil {
+			panic(err)
+		}
+		return fmt.Errorf("Error updating domains for site (%s): %s", d.Get("site_id"), string(out))
+	}
+
 	return resourceDomainRead(d, m)
 }
 
 func resourceDomainRead(d *schema.ResourceData, m interface{}) error {
 	client := m.(*Client)
-	siteDomainDetails, err := client.GetDomainDetails(d.Get("site_id").(string), d.Id())
+	siteDomainDetailsDto, err := client.GetWebsiteDomains(d.Get("site_id").(string))
 	if err != nil {
 		return err
 	}
 
-	d.Set("site_id", siteDomainDetails.SiteId)
+	if siteDomainDetailsDto.Errors != nil && len(siteDomainDetailsDto.Errors) > 0 {
+		if siteDomainDetailsDto.Errors[0].Status == "404" || siteDomainDetailsDto.Errors[0].Status == "400" {
+			log.Printf("[INFO] Operation not allowed: %s\n", siteDomainDetailsDto.Errors)
+			d.SetId("")
+			return nil
+		}
 
+		if siteDomainDetailsDto.Errors[0].Status == "500" {
+			log.Printf("[INFO] Internal Server Error: %s\n", siteDomainDetailsDto.Errors)
+			d.SetId("")
+			return nil
+		}
+
+		out, err := json.Marshal(siteDomainDetailsDto.Errors)
+		if err != nil {
+			panic(err)
+		}
+		return fmt.Errorf("Error getting domains for site (%s): %s", d.Get("site_id"), string(out))
+	}
+
+	domains := &schema.Set{F: getHashFromDomain}
+	for _, v := range siteDomainDetailsDto.Data {
+		if v.MainDomain == true { //we ignore the main domain
+			continue
+		}
+		domain := map[string]interface{}{}
+		domain["domain_name"] = v.Domain
+		domain["id"] = v.Id
+		domain["status"] = v.Status
+		domains.Add(domain)
+	}
+	d.SetId(d.Get("site_id").(string))
+	d.Set("site_id", d.Get("site_id"))
+	d.Set("domain", domains)
 	return nil
 }
 
 func resourceDomainDelete(d *schema.ResourceData, m interface{}) error {
-	return nil
-}
+	client := m.(*Client)
+	siteID := d.Get("site_id").(string)
+	siteDomainDetailsDto, err := client.BulkUpdateDomainsToSite(siteID, []SiteDomainDetails{})
+	if err != nil {
+		return err
+	}
 
-func resourceDomainUpdate(d *schema.ResourceData, m interface{}) error {
-	return nil
+	if siteDomainDetailsDto.Errors != nil && len(siteDomainDetailsDto.Errors) > 0 {
+		if siteDomainDetailsDto.Errors[0].Status == "404" || siteDomainDetailsDto.Errors[0].Status == "400" {
+			log.Printf("[INFO] Operation not allowed: %s\n", siteDomainDetailsDto.Errors)
+			d.SetId("")
+			return nil
+		}
+
+		if siteDomainDetailsDto.Errors[0].Status == "500" {
+			log.Printf("[INFO] Internal Server Error: %s\n", siteDomainDetailsDto.Errors)
+			d.SetId("")
+			return nil
+		}
+
+		out, err := json.Marshal(siteDomainDetailsDto.Errors)
+		if err != nil {
+			panic(err)
+		}
+		return fmt.Errorf("Error deleting domains for site (%s): %s", d.Get("site_id"), string(out))
+	}
+
+	return resourceDomainRead(d, m)
 }
