3d5d00d9070118a9a7b8f220cb8228019d8ea10b
Nir Parizer <nir.parizer@imperva.com>
2022-11-07 14:29:52 +0200

    #CWMS-2016 adding current account id support to incapsula_policy_asset_association

--- incapsula/client_policy_asset_association.go
+++ incapsula/client_policy_asset_association.go
@@ -14,11 +14,14 @@ type PolicyAssetAssociationStatus struct {
 }
 
 // AddPolicyAssetAssociation adds a policy to be managed by Incapsula
-func (c *Client) AddPolicyAssetAssociation(policyID, assetID, assetType string) error {
+func (c *Client) AddPolicyAssetAssociation(policyID, assetID, assetType string, currentAccountId *int) error {
 	log.Printf("[INFO] Adding Incapsula Policy Asset Association: %s/%s/%s\n", policyID, assetID, assetType)
 
 	// Post form to Incapsula
 	reqURL := fmt.Sprintf("%s/policies/v2/assets/%s/%s/policies/%s", c.config.BaseURLAPI, assetType, assetID, policyID)
+	if currentAccountId != nil && *currentAccountId != 0 {
+		reqURL = fmt.Sprintf("%s?caid=%d", reqURL, *currentAccountId)
+	}
 	resp, err := c.DoJsonRequestWithHeaders(http.MethodPost, reqURL, nil, CreatePolicyAssetAssociation)
 	if err != nil {
 		return fmt.Errorf("Error from Incapsula service when adding Policy Asset Association: %s", err)
@@ -40,11 +43,14 @@ func (c *Client) AddPolicyAssetAssociation(policyID, assetID, assetType string) 
 }
 
 // DeletePolicyAssetAssociation deletes a policy asset association currently managed by Incapsula
-func (c *Client) DeletePolicyAssetAssociation(policyID, assetID, assetType string) error {
+func (c *Client) DeletePolicyAssetAssociation(policyID, assetID, assetType string, currentAccountId *int) error {
 	log.Printf("[INFO] Deleting Incapsula Policy Asset Association: %s/%s/%s\n", policyID, assetID, assetType)
 
 	// Delete request to Incapsula
 	reqURL := fmt.Sprintf("%s/policies/v2/assets/%s/%s/policies/%s", c.config.BaseURLAPI, assetType, assetID, policyID)
+	if currentAccountId != nil {
+		reqURL = fmt.Sprintf("%s?caid=%d", reqURL, *currentAccountId)
+	}
 	resp, err := c.DoJsonRequestWithHeaders(http.MethodDelete, reqURL, nil, DeletePolicyAssetAssociation)
 	if err != nil {
 		return fmt.Errorf("Error from Incapsula service when deleting Policy Asset Association (%s): %s", policyID, err)
@@ -65,11 +71,14 @@ func (c *Client) DeletePolicyAssetAssociation(policyID, assetID, assetType strin
 	return nil
 }
 
-func (c *Client) isPolicyAssetAssociated(policyID, assetID, assetType string) (bool, error) {
+func (c *Client) isPolicyAssetAssociated(policyID, assetID, assetType string, currentAccountId *int) (bool, error) {
 	log.Printf("[INFO] Checking Policy Asset Association: %s/%s/%s\n", policyID, assetID, assetType)
 
 	// Check with Policies if the association exist
 	reqURL := fmt.Sprintf("%s/policies/v2/policies/%s/assets/%s/%s", c.config.BaseURLAPI, policyID, assetType, assetID)
+	if currentAccountId != nil {
+		reqURL = fmt.Sprintf("%s?caid=%d", reqURL, *currentAccountId)
+	}
 	resp, err := c.DoJsonRequestWithHeaders(http.MethodGet, reqURL, nil, ReadPolicyAssetAssociation)
 	if err != nil {
 		return false, fmt.Errorf("error from Incapsula service when checking if Policy Asset Association exist: %s/%s/%s, err: %s", policyID, assetID, assetType, err)
@@ -82,7 +91,10 @@ func (c *Client) isPolicyAssetAssociated(policyID, assetID, assetType string) (b
 	log.Printf("[DEBUG] Incapsula isPolicyAssetAssociated for: %s/%s/%s , response is: %s\n", policyID, assetID, assetType, string(responseBody))
 
 	// Check the response code
-	// If policy asset is not associated 404 will be returned from policies
+	if resp.StatusCode == 404 {
+		// If policy asset is not associated 404 will be returned from policies
+		return false, nil
+	}
 	if resp.StatusCode != 200 {
 		return false, fmt.Errorf("Error status code %d from Incapsula service when checking the reading Policy Asset Association: %s/%s/%s, response is: %s", resp.StatusCode, policyID, assetID, assetType, string(responseBody))
 	}
