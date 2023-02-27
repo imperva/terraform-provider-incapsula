32338988162e1d6e59b995b7af41ce21ffcf0618
BrachaY <bracha.yalon@gmail.com>
2023-02-27 21:25:37 +0200

    version 3.15.2

--- incapsula/client.go
+++ incapsula/client.go
@@ -34,7 +34,7 @@ type Client struct {
 func NewClient(config *Config) *Client {
 	client := &http.Client{}
 
-	return &Client{config: config, httpClient: client, providerVersion: "3.15.1"}
+	return &Client{config: config, httpClient: client, providerVersion: "3.15.2"}
 }
 
 func (c *Client) CreateFormDataBody(bodyMap map[string]interface{}) ([]byte, string) {
