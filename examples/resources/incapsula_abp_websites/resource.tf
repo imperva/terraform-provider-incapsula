3a2e6b1fb4e63445d14f0a06502cb2187bb17106
Markus Westerlind <markus.westerlind@imperva.com>
2023-06-30 15:47:41 +0200

    doc: Add a second ABP example

--- examples/resources/incapsula_abp_websites/resource.tf
+++ examples/resources/incapsula_abp_websites/resource.tf
@@ -1,3 +1,4 @@
+
 resource "incapsula_abp_websites" "abp_websites" {
     account_id = data.incapsula_account_data.account_data.current_account
     auto_publish = true
@@ -12,6 +13,31 @@ resource "incapsula_abp_websites" "abp_websites" {
         name = "sites-2"
         website {
             site_id = incapsula_site.sites-2.id
+            enable_mitigation = true
+        }
+    }
+}
+
+
+resource "incapsula_abp_websites" "abp_websites" {
+    account_id = data.incapsula_account_data.account_data.current_account
+    auto_publish = true
+    website_group {
+        name = "sites"
+        website {
+            site_id = incapsula_site.sites-1.id
+            enable_mitigation = false
+        }
+        website {
+            site_id = incapsula_site.sites-2.id
+            enable_mitigation = true
+        }
+    }
+    website_group {
+        name = "sites" # Duplicate name
+        name_id = "sites-2" # name_id can be used to disambiguate names in case of duplicates
+        website {
+            site_id = incapsula_site.sites-3.id
             enable_mitigation = true
         }
     }
