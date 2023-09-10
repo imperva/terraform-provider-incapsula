a2e1d0b67de69bd57a8249f28f57cb47d64c4383
aleksandar.savchev <aleksandar.savchev@imperva.com>
2023-09-10 10:36:53 +0300

    Policy asset association resource typo fix revert.

--- incapsula/resource_policy_asset_association.go
+++ incapsula/resource_policy_asset_association.go
@@ -93,7 +93,7 @@ func resourcePolicyAssetAssociationRead(d *schema.ResourceData, m interface{}) e
 
 	if !isAssociated {
 		log.Printf("[ERROR] Could not find Incapsula Policy Asset Association: %s-%s-%s\n", policyID, assetID, assetType)
-		return fmt.Printf("Incapsula Policy Asset Association doesn't exisits: policy id:%s asset id:%s asset type:%s\n", policyID, assetID, assetType)
+		return fmt.Errorf("Incapsula Policy Asset Association doesn't exisits: policy id:%s asset id:%s asset type:%s\n", policyID, assetID, assetType)
 	}
 
 	log.Printf("[INFO] Successfully read Policy Asset Association exist: %s-%s-%s\n", policyID, assetID, assetType)
