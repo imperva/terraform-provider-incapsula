79c50d3a533fbacad85d1e90189b1398e0bd7dd7
jamil.otman <jamil.otman@imperva.com>
2023-11-12 12:52:17 +0200

    AD Rules priorities - edit component test file

--- incapsula/resource_delivery_rules_configuration_test.go
+++ incapsula/resource_delivery_rules_configuration_test.go
@@ -19,23 +19,73 @@ const redirectCategory = "REDIRECT"
 const rewriteCategory = "REWRITE"
 const forwardCategory = "FORWARD"
 
-
-
 func TestAccIncapsulaDeliveryRule_Basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckIncapsulaDeliveryRuleDestroy,
 		Steps: []resource.TestStep{
-			Config : testAccCheckIncapsulaDeliveryRedirectRuleConfigBasic(t), // working json validation
-			Check: resource.ComposeTestCheckFunc(
-				testCheckIncapsulaIncapRuleExists(deliveryRuleResourceName),
-				resource.TestCheckResourceAttr(redirectRuleName, "name", redirectRuleName),
-				resource.TestCheckResourceAttr(incapRuleResourceName, "enabled", "true"),
-			),
+			{
+				Config: testAccCheckIncapsulaDeliveryRedirectRuleConfigBasic(t),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckIncapsulaDeliveryRuleExists(deliveryRuleResourceName),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "category", redirectCategory),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.rule_name", redirectRuleName),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.enabled", "true"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.action", "RULE_ACTION_REDIRECT"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.filter", "ASN == 1"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.response_code", "302"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.from", "/1"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.to", "/2"),
+				),
+			},
+			{
+				Config: testAccCheckIncapsulaDeliveryRewriteRuleConfigBasic(t),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckIncapsulaDeliveryRuleExists(deliveryRuleResourceName),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "category", rewriteCategory),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.rule_name", rewriteRuleName1),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.enabled", "true"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.action", "RULE_ACTION_REWRITE_COOKIE"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.filter", "ASN == 2"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.rewrite_existing", "true"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.from", "cookie1"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.to", "cookie2"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.add_if_missing", "false"),
+
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.1.rule_name", rewriteRuleName2),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.1.enabled", "true"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.1.action", "RULE_ACTION_REWRITE_HEADER"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.1.filter", "ASN == 3"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.1.rewrite_existing", "true"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.1.from", "header1"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.1.to", "header2"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.1.add_if_missing", "false"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.1.header_name", "abc"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.1.response_code", "302"),
+				),
+			},
+			{
+				Config: testAccCheckIncapsulaDeliveryForwardRuleConfigBasic(t),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckIncapsulaDeliveryRuleExists(deliveryRuleResourceName),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "category", forwardCategory),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.rule_name", forwardRuleName1),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.enabled", "true"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.action", "RULE_ACTION_FORWARD_TO_DC"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.dc_id", "1234"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.0.filter", "ASN == 1"),
+
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.1.rule_name", forwardRuleName2),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.1.enabled", "true"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.1.action", "RULE_ACTION_FORWARD_TO_PORT"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.1.filter", "ASN == 1"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.1.port_forwarding_context", "[Use Header Name/Use Port Value]"),
+					resource.TestCheckResourceAttr(deliveryRuleResourceName, "rule.1.port_forwarding_value", "1234"),
+				),
+			},
 		},
-		}
-	}
+	})
 }
 
 func testAccCheckIncapsulaDeliveryRuleDestroy(state *terraform.State) error {
@@ -71,21 +121,50 @@ func testAccCheckIncapsulaDeliveryRuleDestroy(state *terraform.State) error {
 
 	return nil
 }
+
+func testCheckIncapsulaDeliveryRuleExists(name string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		res, ok := state.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Incapsula Delivery Rule resource not found: %s", name)
+		}
+
+		siteID, ok := res.Primary.Attributes["site_id"]
+		if !ok || siteID == "" {
+			return fmt.Errorf("Incapsula Site ID does not exist ")
+		}
+
+		client := testAccProvider.Meta().(*Client)
+		category, ok := res.Primary.Attributes["category"]
+		deliveryRulesListDTO, diags := client.ReadIncapRulePriorities(siteID, category)
+
+		if !ok {
+			return fmt.Errorf("Rule category : %s ,does not exist for Site ID is : %s ", siteID, category)
+		}
+		if deliveryRulesListDTO.Errors[0].Status != 200 {
+			return fmt.Errorf("Incapsula Delivery Rule: %s (site id: %s) should have received 200 status code", category, siteID)
+		}
+		if diags != nil {
+			return fmt.Errorf("Incapsula Delivery Rule: %s (site id: %s) does not exist", category, siteID)
+		}
+
+		return nil
+	}
+}
 func testAccCheckIncapsulaDeliveryRedirectRuleConfigBasic(t *testing.T) string {
 	return testAccCheckIncapsulaSiteConfigBasic(GenerateTestDomain(t)) + fmt.Sprintf(`
 resource "incapsula_delivery_rules_configuration" "testacc-terraform-redirect-rules" {
   category = %s
-  ite_id = "${incapsula_site.testacc-terraform-site.id}"
+  site_id = "${incapsula_site.testacc-terraform-site.id}"
   rule {
   	rule_name = %s
-  	action = "RULE_ACTION_ALERT"
   	filter = "ASN == 1"
 	response_code = "302"
 	action = "RULE_ACTION_REDIRECT"
   	enabled = true
     from = "/1"
     to = "/2"
-	}`,redirectCategory, redirectRuleName,
+	}`, redirectCategory, redirectRuleName,
 	)
 }
 func testAccCheckIncapsulaDeliveryRewriteRuleConfigBasic(t *testing.T) string {
@@ -113,11 +192,10 @@ rule {
     response_code = 302
     rewrite_existing = "true"
     add_if_missing = "false"
-    rule_name = "New delivery rule"
     action = "RULE_ACTION_REWRITE_HEADER"
     enabled = "true"
   }
-}`,rewriteCategory, rewriteRuleName1, rewriteRuleName2,
+}`, rewriteCategory, rewriteRuleName1, rewriteRuleName2,
 	)
 }
 func testAccCheckIncapsulaDeliveryForwardRuleConfigBasic(t *testing.T) string {
@@ -129,7 +207,6 @@ resource "incapsula_delivery_rules_configuration" "testacc-terraform-ewrite-rule
   	rule_name = %s
     filter = "ASN == 1"
     dc_id = 1234
-    rule_name = "New delivery rule",
     action = "RULE_ACTION_FORWARD_TO_DC"
     enabled = "true"
   }
@@ -138,10 +215,9 @@ rule {
 	filter = "ASN == 1"
     port_forwarding_context = "[Use Header Name/Use Port Value]"
     port_forwarding_value = 1234
-    rule_name = "New delivery rule"
     action = "RULE_ACTION_FORWARD_TO_PORT"
     enabled = "true"
   }
-}`,forwardCategory ,forwardRuleName1, forwardRuleName2,
+}`, forwardCategory, forwardRuleName1, forwardRuleName2,
 	)
 }
