378791b731088994b7f95885791a251f4f31e720
adishlomo <adishlomo22@gmail.com>
2024-01-30 15:05:06 +0200

    Added 2 optional attributes to account and subaccount resources: (#388)
    * enable_http2_for_new_sites - enable HTTP/2 support for traffic between end-users (visitors) and Imperva for newly created SSL sites.
    * enable_http2_to_origin_for_new_sites - enable HTTP/2 support for traffic between Imperva and origin server for newly created SSL sites.
    
    Co-authored-by: adi.shlomo <adi.shlomo@imperva.com>

--- incapsula/resource_account_test.go
+++ incapsula/resource_account_test.go
@@ -2,39 +2,22 @@ package incapsula
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-const testEmail = "example@example.com"
+const testEmail = "example@imperva.com"
 const accountResourceName = "incapsula_account.test-terraform-account"
 
-func GenerateTestEmail(t *testing.T) string {
-	if v := os.Getenv("INCAPSULA_API_ID"); v == "" {
-		t.Fatal("INCAPSULA_API_ID must be set for acceptance tests")
-	}
-	return "id" + os.Getenv("INCAPSULA_API_ID") + "." + testEmail
-}
-
-func SkipIfAccountTypeIsResellerEndUser(t *testing.T) resource.ErrorCheckFunc {
-	return func(err error) error {
-		if err == nil {
-			return nil
-		}
-		if strings.Contains(err.Error(), "Operation not allowed") {
-			t.Skipf("skipping test since account type is RESELLER_END_USER. Error: %s", err.Error())
-		}
-
-		return err
-	}
-}
-
 func TestIncapsulaAccount_Basic(t *testing.T) {
+	email := GenerateTestEmail(t)
 	resource.Test(t, resource.TestCase{
 		ErrorCheck:   SkipIfAccountTypeIsResellerEndUser(t),
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -42,10 +25,10 @@ func TestIncapsulaAccount_Basic(t *testing.T) {
 		CheckDestroy: testCheckIncapsulaAccountDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testCheckIncapsulaAccountConfigBasic(GenerateTestEmail(t)),
+				Config: testCheckIncapsulaAccountConfigBasic(email),
 				Check: resource.ComposeTestCheckFunc(
 					testCheckIncapsulaAccountExists(accountResourceName),
-					resource.TestCheckResourceAttr(accountResourceName, "email", GenerateTestEmail(t)),
+					resource.TestCheckResourceAttr(accountResourceName, "email", email),
 				),
 			},
 		},
@@ -66,6 +49,40 @@ func TestIncapsulaAccount_ImportBasic(t *testing.T) {
 				ResourceName:      "incapsula_account.test-terraform-account",
 				ImportState:       true,
 				ImportStateVerify: true,
+				ImportStateIdFunc: testACCStateAccountID,
+			},
+		},
+	})
+}
+
+func TestIncapsulaAccount_Http2Defaults(t *testing.T) {
+	testIncapsulaAccountHttp2Client(t, true, false)
+}
+
+func TestIncapsulaAccount_Http2ClientAndOriginEnabled(t *testing.T) {
+	testIncapsulaAccountHttp2Client(t, true, true)
+}
+
+func TestIncapsulaAccount_Http2ClientAndOriginDisabled(t *testing.T) {
+	testIncapsulaAccountHttp2Client(t, false, false)
+}
+
+func testIncapsulaAccountHttp2Client(t *testing.T, enableHttp2ForNewSites bool, enableHttp2ToOriginForNewSites bool) {
+	email := GenerateTestEmail(t)
+	resource.Test(t, resource.TestCase{
+		ErrorCheck:   SkipIfAccountTypeIsResellerEndUser(t),
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckIncapsulaAccountDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testHttp2AccountConfig(email, enableHttp2ForNewSites, enableHttp2ToOriginForNewSites),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckIncapsulaAccountExists(accountResourceName),
+					resource.TestCheckResourceAttr(accountResourceName, "email", email),
+					resource.TestCheckResourceAttr(accountResourceName, "enable_http2_for_new_sites", strconv.FormatBool(enableHttp2ForNewSites)),
+					resource.TestCheckResourceAttr(accountResourceName, "enable_http2_to_origin_for_new_sites", strconv.FormatBool(enableHttp2ToOriginForNewSites)),
+				),
 			},
 		},
 	})
@@ -128,9 +145,66 @@ func testCheckIncapsulaAccountExists(name string) resource.TestCheckFunc {
 
 func testCheckIncapsulaAccountConfigBasic(email string) string {
 	return fmt.Sprintf(`
-		resource "incapsula_account" "test-terraform-account" {
+			resource "incapsula_account" "test-terraform-account" {
 			email = "%s"
+            account_name = "testTerraform"
+            plan_id = "entTrial"
+            support_all_tls_versions = "false"
+			naked_domain_san_for_new_www_sites = "true"
 		}`,
 		email,
 	)
+}
+
+func testHttp2AccountConfig(email string, enableHttp2ForNewSites bool, enableHttp2ToOriginForNewSites bool) string {
+	return fmt.Sprintf(`
+		resource "incapsula_account" "test-terraform-account" {
+			email = "%s"
+			enable_http2_for_new_sites = "%t"
+			enable_http2_to_origin_for_new_sites = "%t"
+            account_name = "testTerraform"
+            plan_id = "entTrial"
+            support_all_tls_versions = "false"
+		}`,
+		email,
+		enableHttp2ForNewSites,
+		enableHttp2ToOriginForNewSites,
+	)
+}
+
+func SkipIfAccountTypeIsResellerEndUser(t *testing.T) resource.ErrorCheckFunc {
+	return func(err error) error {
+		if err == nil {
+			return nil
+		}
+		if strings.Contains(err.Error(), "Operation not allowed") {
+			t.Skipf("skipping test since account type is RESELLER_END_USER. Error: %s", err.Error())
+		}
+
+		return err
+	}
+}
+
+func GenerateTestEmail(t *testing.T) string {
+	if v := os.Getenv("INCAPSULA_API_ID"); v == "" {
+		t.Fatal("INCAPSULA_API_ID must be set for acceptance tests")
+	}
+
+	s3 := rand.NewSource(time.Now().UnixNano())
+	r3 := rand.New(s3)
+	generatedDomain = "id" + os.Getenv("INCAPSULA_API_ID") + strconv.Itoa(r3.Intn(1000)) + testEmail
+
+	return generatedDomain
+}
+
+func testACCStateAccountID(s *terraform.State) (string, error) {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "incapsula_account" {
+			continue
+		}
+		accountID := rs.Primary.ID
+
+		return accountID, nil
+	}
+	return "", fmt.Errorf("Error finding an Account\"")
 }
