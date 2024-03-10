49d48fa7d5d34334c5f5e95457fcce1106b82d97
Jonathan E <119786589+jonathan-el@users.noreply.github.com>
2024-03-10 19:14:56 +0200

    Delivery Rules Configuration resource (#384)
    * Add delivery rules configuration resource
    
    * valus fix
    
    * Update delivery_rules_configuration.markdown
    
    * Update simplified_redirect_rules_configuration.markdown
    
    * fix docs
    
    * fix docs
    
    * Change resource_simplified_redirect_rules_configuration to list type

---
+++ incapsula/resource_simplified_redirect_rules_configuration.go
@@ -0,0 +1,194 @@
+package incapsula
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+func resourceSimplifiedRedirectRulesConfiguration() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceSimplifiedRedirectRulesConfigurationUpdate,
+		ReadContext:   resourceSimplifiedRedirectRulesConfigurationRead,
+		UpdateContext: resourceSimplifiedRedirectRulesConfigurationUpdate,
+		DeleteContext: resourceSimplifiedRedirectRulesConfigurationDelete,
+		Importer: &schema.ResourceImporter{
+			State: func(data *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+				idSlice := strings.Split(data.Id(), "/")
+				if len(idSlice) != 1 || idSlice[0] == "" {
+					return nil, fmt.Errorf("unexpected format of ID (%q), expected site_id", data.Id())
+				}
+
+				data.Set("site_id", idSlice[0])
+				data.SetId(idSlice[0])
+
+				return []*schema.ResourceData{data}, nil
+			},
+		},
+
+		Schema: map[string]*schema.Schema{
+			// Required Arguments
+			"site_id": {
+				Description: "Numeric identifier of the site to operate on.",
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+			},
+
+			"rule": {
+				Description: "List of simplified redirect rules",
+				Optional:    true,
+				Type:        schema.TypeList,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"rule_name": {
+							Type:        schema.TypeString,
+							Description: "The rule name",
+							Required:    true,
+						},
+
+						"enabled": {
+							Type:        schema.TypeBool,
+							Description: "Boolean that enables the rule",
+							Optional:    true,
+							Default:     true,
+						},
+
+						"from": {
+							Type:        schema.TypeString,
+							Description: "From value",
+							Required:    true,
+						},
+
+						"to": {
+							Type:        schema.TypeString,
+							Description: "To value",
+							Required:    true,
+						},
+
+						"response_code": {
+							Type:             schema.TypeInt,
+							Description:      "Rule's response code",
+							Required:         true,
+							ValidateDiagFunc: validation.ToDiagFunc(validation.IntInSlice([]int{301, 302, 303, 307, 308})),
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceSimplifiedRedirectRulesConfigurationUpdate(ctx context.Context, data *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*Client)
+	siteID := data.Get("site_id").(string)
+
+	rulesListDTO := DeliveryRulesListDTO{
+		RulesList: createSimplifiedRedirectRulesListFromState(data),
+	}
+
+	simplifiedRediectRulesListDTO, diags := client.UpdateDeliveryRuleConfiguration(siteID, "SIMPLIFIED_REDIRECT", &rulesListDTO)
+
+	if diags != nil && diags.HasError() {
+		log.Printf("[ERROR] Failed to update delivery rules of category SIMPLIFIED_REDIRECT for Site ID %s", siteID)
+		return diags
+	} else if simplifiedRediectRulesListDTO.Errors != nil {
+		errors, _ := json.Marshal(simplifiedRediectRulesListDTO.Errors)
+		log.Printf("[ERROR] Failed to update delivery rules of category SIMPLIFIED_REDIRECT for Site ID %s: %s", siteID, string(errors[:]))
+		return []diag.Diagnostic{{
+			Severity: diag.Error,
+			Summary:  "Failed to update simplified redirect rules",
+			Detail:   fmt.Sprintf("Failed to update delivery rules of category SIMPLIFIED_REDIRECT for Site ID %s: %s", siteID, string(errors[:])),
+		}}
+	}
+
+	diags = append(diags, resourceSimplifiedRedirectRulesConfigurationRead(ctx, data, m)[:]...)
+	return diags
+}
+
+func resourceSimplifiedRedirectRulesConfigurationRead(ctx context.Context, data *schema.ResourceData, m interface{}) diag.Diagnostics {
+	// Implement by reading the SiteResponse for the site
+	client := m.(*Client)
+	siteID := data.Get("site_id").(string)
+
+	simplifiedRediectRulesListDTO, diags := client.ReadDeliveryRuleConfiguration(siteID, "SIMPLIFIED_REDIRECT")
+
+	fmt.Println(simplifiedRediectRulesListDTO)
+
+	if simplifiedRediectRulesListDTO != nil && simplifiedRediectRulesListDTO.Errors != nil && simplifiedRediectRulesListDTO.Errors[0].Status == 404 {
+		log.Printf("[INFO] Incapsula Site with ID %s has already been deleted\n", data.Get("site_id"))
+		data.SetId("")
+		return nil
+	}
+	if diags != nil && diags.HasError() {
+		log.Printf("[ERROR] Failed to read delivery rules in category SIMPLIFIED_REDIRECT for Site ID %s", siteID)
+		return diags
+	}
+
+	data.Set("rule", serializeSimplifiedRedirectRule(data, *simplifiedRediectRulesListDTO))
+	data.SetId(siteID)
+
+	return nil
+}
+
+func serializeSimplifiedRedirectRule(data *schema.ResourceData, DeliveryRule DeliveryRulesListDTO) []interface{} {
+	simplifiedRedirectRules := make([]interface{}, len(DeliveryRule.RulesList))
+
+	for i, rule := range DeliveryRule.RulesList {
+		simplifiedRedirectRuleSlice := make(map[string]interface{})
+		simplifiedRedirectRuleSlice["rule_name"] = rule.RuleName
+		simplifiedRedirectRuleSlice["from"] = rule.From
+		simplifiedRedirectRuleSlice["to"] = rule.To
+		simplifiedRedirectRuleSlice["response_code"] = rule.ResponseCode
+		simplifiedRedirectRuleSlice["enabled"] = rule.Enabled
+		simplifiedRedirectRules[i] = simplifiedRedirectRuleSlice
+	}
+
+	return simplifiedRedirectRules
+}
+
+func resourceSimplifiedRedirectRulesConfigurationDelete(ctx context.Context, data *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*Client)
+	var diags diag.Diagnostics
+	siteID := data.Get("site_id").(string)
+
+	emptyRulesList := DeliveryRulesListDTO{
+		RulesList: []DeliveryRuleDto{},
+	}
+
+	_, diags = client.UpdateDeliveryRuleConfiguration(siteID, "SIMPLIFIED_REDIRECT", &emptyRulesList)
+	if diags != nil && diags.HasError() {
+		log.Printf("[ERROR] Failed to delete delivery rules in category SIMPLIFIED_REDIRECT for Site ID %s", siteID)
+		return diags
+	}
+
+	data.SetId("")
+	return diags
+}
+
+func createSimplifiedRedirectRulesListFromState(data *schema.ResourceData) []DeliveryRuleDto {
+	simplifiedRediectRulesListConf := data.Get("rule").([]interface{})
+	simplifiedRediectRulesListDTO := make([]DeliveryRuleDto, len(simplifiedRediectRulesListConf))
+
+	for i, simplifiedRediectRuleRaw := range simplifiedRediectRulesListConf {
+		simplifiedRediectRule := simplifiedRediectRuleRaw.(map[string]interface{})
+
+		simplifiedRediectRuleDTO := DeliveryRuleDto{
+			RuleName:     simplifiedRediectRule["rule_name"].(string),
+			Action:       "RULE_ACTION_SIMPLIFIED_REDIRECT",
+			Enabled:      simplifiedRediectRule["enabled"].(bool),
+			From:         simplifiedRediectRule["from"].(string),
+			To:           simplifiedRediectRule["to"].(string),
+			ResponseCode: simplifiedRediectRule["response_code"].(int),
+		}
+		simplifiedRediectRulesListDTO[i] = simplifiedRediectRuleDTO
+	}
+
+	return simplifiedRediectRulesListDTO
+}
