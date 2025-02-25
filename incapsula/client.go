b45af27269f55b4f07b6ea3ad799715cd7f33cb5
bracha.yalon <bracha.yalon@imperva.com>
2025-02-25 09:51:18 +0200

    Bump version: 3.29.3

--- incapsula/client.go
+++ incapsula/client.go
@@ -34,7 +34,7 @@ type Client struct {
 func NewClient(config *Config) *Client {
 	client := &http.Client{}
 
-	return &Client{config: config, httpClient: client, providerVersion: "3.29.1"}
+	return &Client{config: config, httpClient: client, providerVersion: "3.29.3"}
 }
 
 func (c *Client) CreateFormDataBody(bodyMap map[string]interface{}) ([]byte, string) {
