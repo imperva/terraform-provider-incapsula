53d8ac728d4d4dbf02edf7f2da964e779b0164b4
karin.yechilzuke <karin.yechilzuke@imperva.com>
2026-01-19 12:35:23 +0200

    updated const to be inside the tests and not as global

--- incapsula/resource_site_ssl_settings_test.go
+++ incapsula/resource_site_ssl_settings_test.go
@@ -9,13 +9,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-// NOTE: SSL settings endpoint requires the site to have SSL configured with a custom certificate.
-// This test uploads a self-signed certificate before configuring SSL settings.
-const sslSettingsResourceType = "incapsula_site_ssl_settings"
-const sslSettingsResourceName = "testacc-terraform-site-ssl-settings"
-const sslSettingsFullResourceName = sslSettingsResourceType + "." + sslSettingsResourceName
-
 func TestAccSiteSSLSettings_Basic(t *testing.T) {
+	const sslSettingsFullResourceName = "incapsula_site_ssl_settings.testacc-terraform-site-ssl-settings"
 	domainName = GenerateTestDomain(t)
 	cert, pkey := generateKeyPairBase64(domainName)
 	resource.Test(t, resource.TestCase{
@@ -26,7 +21,7 @@ func TestAccSiteSSLSettings_Basic(t *testing.T) {
 			{
 				Config: testSSLSettingsConfig(domainName, cert, pkey, 31536000, true, true, true),
 				Check: resource.ComposeTestCheckFunc(
-					testCheckIncapsulaIncapSiteSSLSettingsExists(),
+					testCheckIncapsulaIncapSiteSSLSettingsExists(sslSettingsFullResourceName),
 					resource.TestCheckResourceAttr(sslSettingsFullResourceName, "hsts.0.is_enabled", "true"),
 					resource.TestCheckResourceAttr(sslSettingsFullResourceName, "hsts.0.max_age", "31536000"),
 					resource.TestCheckResourceAttr(sslSettingsFullResourceName, "hsts.0.sub_domains_included", "true"),
@@ -43,6 +38,7 @@ func TestAccSiteSSLSettings_Basic(t *testing.T) {
 }
 
 func TestAccSiteSSLSettings_Update(t *testing.T) {
+	const sslSettingsFullResourceName = "incapsula_site_ssl_settings.testacc-terraform-site-ssl-settings"
 	domainName = GenerateTestDomain(t)
 	cert, pkey := generateKeyPairBase64(domainName)
 	resource.Test(t, resource.TestCase{
@@ -53,7 +49,7 @@ func TestAccSiteSSLSettings_Update(t *testing.T) {
 			{
 				Config: testSSLSettingsConfig(domainName, cert, pkey, 31536000, true, true, true),
 				Check: resource.ComposeTestCheckFunc(
-					testCheckIncapsulaIncapSiteSSLSettingsExists(),
+					testCheckIncapsulaIncapSiteSSLSettingsExists(sslSettingsFullResourceName),
 					resource.TestCheckResourceAttr(sslSettingsFullResourceName, "hsts.0.max_age", "31536000"),
 					resource.TestCheckResourceAttr(sslSettingsFullResourceName, "hsts.0.sub_domains_included", "true"),
 					resource.TestCheckResourceAttr(sslSettingsFullResourceName, "hsts.0.pre_loaded", "true"),
@@ -63,7 +59,7 @@ func TestAccSiteSSLSettings_Update(t *testing.T) {
 			{
 				Config: testSSLSettingsConfig(domainName, cert, pkey, 86400, false, false, false),
 				Check: resource.ComposeTestCheckFunc(
-					testCheckIncapsulaIncapSiteSSLSettingsExists(),
+					testCheckIncapsulaIncapSiteSSLSettingsExists(sslSettingsFullResourceName),
 					resource.TestCheckResourceAttr(sslSettingsFullResourceName, "hsts.0.max_age", "86400"),
 					resource.TestCheckResourceAttr(sslSettingsFullResourceName, "hsts.0.sub_domains_included", "false"),
 					resource.TestCheckResourceAttr(sslSettingsFullResourceName, "hsts.0.pre_loaded", "false"),
@@ -113,9 +109,9 @@ func testAccSiteCheckIncapsulaSiteSSLSettingsDestroy(state *terraform.State) err
 	return nil
 }
 
-func testCheckIncapsulaIncapSiteSSLSettingsExists() resource.TestCheckFunc {
+func testCheckIncapsulaIncapSiteSSLSettingsExists(resourceName string) resource.TestCheckFunc {
 	return func(state *terraform.State) error {
-		res, ok := state.RootModule().Resources[sslSettingsFullResourceName]
+		res, ok := state.RootModule().Resources[resourceName]
 		if !ok {
 			return fmt.Errorf("incapsula Site SSL settings resource not found")
 		}
@@ -152,6 +148,8 @@ func testCheckIncapsulaIncapSiteSSLSettingsExists() resource.TestCheckFunc {
 	}
 }
 
+// NOTE: SSL settings endpoint requires the site to have SSL configured with a custom certificate.
+// This test uploads a self-signed certificate before configuring SSL settings.
 func testSSLSettingsConfig(domain, cert, pkey string, maxAge int, subDomainsIncluded, preLoaded, disablePQC bool) string {
 	return fmt.Sprintf(`
 		resource "incapsula_site" "testacc-terraform-site" {
