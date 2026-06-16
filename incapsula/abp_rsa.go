7950b5777e01c585b12b47e6df1e68f6f2eb7dcf
MarcusGrass <marcus.grass@protonmail.com>
2026-06-16 11:12:53 +0200

    fix: handle invalid RSA keys and properly report errors

--- incapsula/abp_rsa.go
+++ incapsula/abp_rsa.go
@@ -17,14 +17,18 @@ func encryptRsa(rsaPem []byte, plaintext []byte, encLabel string) (string, error
 		return "", fmt.Errorf("error decoding RSA PEM no entities found")
 	}
 	key, err := x509.ParsePKIXPublicKey(block.Bytes)
-	var rsaKey = key.(*rsa.PublicKey)
+	if err != nil {
+		return "", fmt.Errorf("error parsing RSA public key: %w", err)
+	}
+	rsaKey, ok := key.(*rsa.PublicKey)
+	if !ok {
+		return "", fmt.Errorf("error parsing RSA public key, not RSA")
+	}
 	var bits = rsaKey.Size() * 8
 	if bits < 3072 {
 		return "", fmt.Errorf("RSA key size must be at least 3072 bits, got %d bits", bits)
 	}
-	if err != nil {
-		return "", fmt.Errorf("error parsing RSA public key: %w", err)
-	}
+
 	encrypted, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, rsaKey, plaintext, []byte(encLabel))
 	if err != nil {
 		return "", fmt.Errorf("error encrypting value with RSA: %w", err)
